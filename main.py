f92720b706979d71b489f9653205cfd4c26fdadd
FSB-PC <homkkarsx@gmail.com>
2022-11-30 10:47:47 +0300

    done v1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageOps
 
 #read the image
 im = Image.open("image.jpg")
@@ -6,14 +6,19 @@ im = Image.open("image.jpg")
 #image brightness enhancer
 enhancer = ImageEnhance.Brightness(im)
 
-factor = 1 #gives original image
-im_output = enhancer.enhance(factor)
-im_output.save('original-image.png')
+#image invert color enhancer
+im = Image.open('image.jpg')
+im_invert = ImageOps.invert(im)
+im_invert.save('invert.jpg', quality=95)
 
-factor = 0.5 #darkens the image
-im_output = enhancer.enhance(factor)
-im_output.save('darkened-image.png')
-
-factor = 1.5 #brightens the image
-im_output = enhancer.enhance(factor)
-im_output.save('brightened-image.png')
+# factor = 1 #gives original image
+# im_output = enhancer.enhance(factor)
+# im_output.save('original-image.png')
+#
+# factor = 0.5 #darkens the image
+# im_output = enhancer.enhance(factor)
+# im_output.save('darkened-image.png')
+#
+# factor = 1.5 #brightens the image
+# im_output = enhancer.enhance(factor)
+# im_output.save('brightened-image.png')
